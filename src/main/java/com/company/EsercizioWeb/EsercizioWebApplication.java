f88b8937dd1189f0d568c726131c6e12421463c3
Krahll <andrea.innacolo@gmail.com>
2021-11-08 11:11:11 +0100

    Initial commit

---
+++ src/main/java/com/company/EsercizioWeb/EsercizioWebApplication.java
@@ -0,0 +1,13 @@
+package com.company.EsercizioWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/EsercizioWeb/EsercizioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.EsercizioWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
