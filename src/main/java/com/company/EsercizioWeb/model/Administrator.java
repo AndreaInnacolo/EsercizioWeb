5c65fc7e5e65dffd7cb01e32ff4445436331160c
gianlucazinna5 <gianluca.zinna96@gmail.com>
2021-11-09 10:00:17 +0100

    enum e classi 2

--- src/main/java/com/company/EsercizioWeb/model/Administrator.java
+++ src/main/java/com/company/EsercizioWeb/model/Administrator.java
@@ -9,5 +9,24 @@ public class Administrator {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Priority getLevel() {
+        return level;
+    }
 
 }
